a1edb7bc0d295799821a636446251164e591d6da
beqa <beqa.tskhvediani@gmail.com>
2024-02-23 15:53:07 +0400

    add css components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import First from "./components/first/First";
+import Second from "./components/second/Second";
+function App() {
+  return (
+    <>
+      <First />
+      <Second />
+    </>
+  );
+}
+
+export default App;
