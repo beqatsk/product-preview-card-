c5dbab556e7c89a2950f73578d3032fdff7701b0
beqa <beqa.tskhvediani@gmail.com>
2024-02-24 11:00:40 +0400

    add price title

--- src/components/first/First.jsx
+++ src/components/first/First.jsx
@@ -12,6 +12,10 @@ export default function First() {
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL.
         </p>
+        <div className="price-title">
+          <span className="new-price">$149.99</span>
+          <span className="old-price">$169.99</span>
+        </div>
       </div>
     </div>
   );
